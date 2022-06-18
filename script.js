59cf1e6e91d7e319dded17ef84d7bf39e25d60af
José Victor <victdiasmd@gmail.com>
2022-06-17 21:23:51 -0300

    Segunda aula

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.querySelector("#root"));
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
+ReactDOM.render(
+    <ul>
+        <li>First Item</li>
+        <li>Second Item</li>
+    </ul>,
+    document.querySelector("#root")
+);
